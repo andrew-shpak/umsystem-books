1bba18c064d8a8a6e1291a07f8be704dbb0f7a0f
Andrew Shpak <andrii.shpak@outlook.com>
2022-06-11 22:30:18 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExamplePdfViewerComponent } from './example-pdf-viewer/example-pdf-viewer.component';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamplePdfViewerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxExtendedPdfViewerModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/example-pdf-viewer/example-pdf-viewer.component.ts
@@ -0,0 +1,37 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-example-pdf-viewer',
+  templateUrl: './example-pdf-viewer.component.html',
+  styleUrls: ['./example-pdf-viewer.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExamplePdfViewerComponent {
+  public src: string | ArrayBuffer | Blob | Uint8Array | URL | { range: any; } ="";
+  /** In most cases, you don't need the NgxExtendedPdfViewerService. It allows you
+   *  to use the "find" api, to extract text and images from a PDF file,
+   *  to print programmatically, and to show or hide layers by a method call.
+  */
+  constructor(private http: HttpClient, private pdfService: NgxExtendedPdfViewerService) {
+    /* More likely than not you don't need to tweak the pdfDefaultOptions.
+       They are a collecton of less frequently used options.
+       To illustrate how they're used, here are two example settings: */
+    pdfDefaultOptions.doubleTapZoomFactor = '150%'; // The default value is '200%'
+    pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5; // The default value is 4096 * 4096 pixels,
+    // but most devices support much higher resolutions.
+    // Increasing this setting allows your users to use higher zoom factors,
+    // trading image quality for performance.
+    this.loadLargeFile();
+    }
+
+  public loadLargeFile(): void {
+    this.http
+      .get(
+        'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf',
+        { responseType: 'blob' }
+      )
+      .subscribe((res) => this.src = res);
+  }
+}

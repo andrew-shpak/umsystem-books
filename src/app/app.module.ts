eb128d2981fe53db7d5eb6c196d968dbfb9e9df5
Andrew Shpak <andrii.shpak@outlook.com>
2022-06-19 15:59:34 +0300

    update book src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExamplePdfViewerComponent } from './example-pdf-viewer/example-pdf-viewer.component';
+import {
+  ExamplePdfViewerComponent,
+} from './example-pdf-viewer/example-pdf-viewer.component';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
--- src/app/example-pdf-viewer/example-pdf-viewer.component.ts
+++ src/app/example-pdf-viewer/example-pdf-viewer.component.ts
@@ -1,5 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { NgxExtendedPdfViewerService, pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
@@ -9,28 +8,18 @@ import {HttpClient} from "@angular/common/http";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ExamplePdfViewerComponent {
-  public src: string | ArrayBuffer | Blob | Uint8Array | URL | { range: any; } ="";
-  /** In most cases, you don't need the NgxExtendedPdfViewerService. It allows you
-   *  to use the "find" api, to extract text and images from a PDF file,
-   *  to print programmatically, and to show or hide layers by a method call.
-  */
-  constructor(private http: HttpClient, private pdfService: NgxExtendedPdfViewerService) {
-    /* More likely than not you don't need to tweak the pdfDefaultOptions.
-       They are a collecton of less frequently used options.
-       To illustrate how they're used, here are two example settings: */
-    pdfDefaultOptions.doubleTapZoomFactor = '150%'; // The default value is '200%'
-    pdfDefaultOptions.maxCanvasPixels = 4096 * 4096 * 5; // The default value is 4096 * 4096 pixels,
-    // but most devices support much higher resolutions.
-    // Increasing this setting allows your users to use higher zoom factors,
-    // trading image quality for performance.
+  public src: Blob | string = "";
+
+  constructor(private http: HttpClient) {
     this.loadLargeFile();
-    }
+  }
+
 
   public loadLargeFile(): void {
     this.http
       .get(
-        'https://f003.backblazeb2.com/file/umsystem-books/%2B%2B+004+Буров+Є.В.++Комп\'ютерні+мережі.pdf',
-        { responseType: 'blob' }
+        '/assets/pdfs/check.pdf',
+        {responseType: 'blob'}
       )
       .subscribe((res) => this.src = res);
   }
